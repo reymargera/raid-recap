7ce1177ab2c9142f0c2c61a6d2fc7c9b9fd89088
reymargera <javierv2@uci.edu>
2024-08-14 19:41:45 -0700

    Serve images directly

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,12 @@
 const nextConfig = {
     output: 'export',
     basePath: '/raid-recap',
+    images: {
+      unoptimized: true,
+    },
     experimental: {
         cpus: 1
-    }
+    },
 };
 
 export default nextConfig;
