4f241cb703daf9922d7bed28160ef86bbc3b6d9a
reymargera <javierv2@uci.edu>
2025-02-22 22:37:38 -0800

    Add GTag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Raid Team Recap",
-  description: "Goofy raid team stats post season end",
+  title: "Raid Recap",
+  description: "WoW Raid Team Post Tier Recap",
 };
 
 export default function RootLayout({
@@ -16,7 +17,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-D4FYZT7LT3"
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-D4FYZT7LT3');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
